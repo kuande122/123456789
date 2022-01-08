871d39674a54d39470d027176f14dd8e070d4fdf
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-08 23:59:27 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -16,7 +16,7 @@ if confirm_input:
  #st.session_state.start,st.session_state.end = 1,100
  st.write('c=', st.session_state.c)
 #st.session_state.start,st.session_state.end
-x=st.number_input("請輸入%g到%g之間的整數:"(a,b)) 
+x=st.number_input("請輸入%g到%g之間的整數:"%(a,b)) 
 st.write("「輸入確認」鍵記得按兩次喔,否則可能導致程式無法正常運行!") 
 if st.button('輸入確認'):
   if x==st.session_state.c: #如果輸入=答案
