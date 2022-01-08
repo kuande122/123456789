7a2ef24ba757021f6055ff83d91d9d489c5179f7
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-08 14:25:18 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -11,7 +11,7 @@ if confirm_input:
  st.session_state.c = c = random.randint(2,99)
  st.session_state.start = 1
  st.session_state.end = 100
- st.session_state.start,st.session_state.end = 1,100
+ #st.session_state.start,st.session_state.end = 1,100
  st.write('c=', st.session_state.c)
 
 x=st.number_input("請輸入%g到%g之間的整數:"%(st.session_state.start,st.session_state.end)) 
