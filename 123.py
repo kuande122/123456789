0152529ffb7eb7cae56ff109aaf8276f7439df50
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-08 13:13:19 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -9,17 +9,14 @@ if "end" not in st.session_state:
   st.session_state.end = 100
 if "c" not in st.session_state:
   st.session_state.c = c = random.randint(2,99)
-x=st.number_input("請輸入%g到%g之間的整數:"%(st.session_state.start,st.session_state.end))
 
 if confirm_input:
   st.session_state.c = c = random.randint(2,99)
-  st.session_state.start = 1
-  st.session_state.end = 100
-  st.session_state.start,st.session_state.end = 1,100
   st.write('c=', st.session_state.c)
 
   
 confirm_input2 = st.button('輸入確認')
+x=st.number_input("請輸入%g到%g之間的整數:"%(st.session_state.start,st.session_state.end))
 
 
 
