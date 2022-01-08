5d54fea5dedd53ddea60c7227e3875cf9ba1ca8d
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-08 14:18:26 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -3,9 +3,9 @@ import streamlit as st
 confirm_input = st.button('確認產生答案')
 if "start" not in st.session_state:
   st.session_state.start = 1
-if "end" not in st.session_state:
+elif "end" not in st.session_state:
   st.session_state.end = 100
-if "c" not in st.session_state:
+elif "c" not in st.session_state:
   st.session_state.c = random.randint(2,99) 
 if confirm_input:
  st.session_state.c = c = random.randint(2,99)
