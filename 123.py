103eac9d7bcedf613cb5bbc55f02da3bebc8ff34
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-08 13:41:10 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -5,6 +5,8 @@ if "start" not in st.session_state:
   st.session_state.start = 1
 if "end" not in st.session_state:
   st.session_state.end = 100
+if "" not in st.session_state:
+  st.session_state.c = random.randint(2,99) 
 if confirm_input:
  st.session_state.c = c = random.randint(2,99)
  st.session_state.start = 1
