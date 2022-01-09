7d03e647be0b2268ab4bdc102f163b4220b8c884
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-10 00:24:59 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -32,7 +32,7 @@ if confirm_input:
 st.write('c=', st.session_state.c)
 
 #x=st.sidebar.text_input("請輸入%g到%g之間的整數:"%(st.session_state.start,st.session_state.end)) 
-x=st.number_input("請輸入整數?", 0)
+x=st.number_input("請輸入整數?", value=0)
 
  
 if x>st.session_state.c:
