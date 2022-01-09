4d204e27f28312c47a97f67b1ba5367cefc429e8
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-10 00:09:01 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -61,8 +61,7 @@ if x==st.session_state.c and st.session_state.begin == 'y':
  st.markdown(
  f'<img src="data:image/gif;base64,{data_url}" alt="output_ntyylX gif">',
  unsafe_allow_html=True,
- pip install --upgrade streamlit
- streamlit hello
+ reset
  )
 
 if x>st.session_state.c and st.session_state.begin == 'y' and x>0:
