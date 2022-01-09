628c753557e833ae49438fdaa1da8a08dd5e6f65
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-09 23:54:46 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -58,6 +58,7 @@ if x==st.session_state.c and st.session_state.begin == 'y':
  st.markdown(
  f'<img src="data:image/gif;base64,{data_url}" alt="output_ntyylX gif">',
  unsafe_allow_html=True,
+ 
  )
 
 if x>st.session_state.c and st.session_state.begin == 'y' and x>0:
